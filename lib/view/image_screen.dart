5daa3e6fce570ff5033ee2afb564e1544342e07d
Taha Sabiri <121357512+Taha-sabiri@users.noreply.github.com>
2023-08-09 12:18:29 +0330

    sec commit

---
+++ lib/view/image_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImgScreen extends StatefulWidget {
+  const ImgScreen({super.key});
+
+  @override
+  State<ImgScreen> createState() => _ImgScreenState();
+}
+
+class _ImgScreenState extends State<ImgScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
